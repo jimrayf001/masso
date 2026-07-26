35fdc76d864a86c1fffc2c7f3993fada7e2f7e61
jimrayf001 <jimrayf@gmail.com>
2026-07-26 12:47:42 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const categoriasInfo = {
   vip: { titulo: "👑 VIP", clase: "seccion-vip" },
   "super-premium": { titulo: "✨ Súper Premium", clase: "seccion-super" },
   premium: { titulo: "⭐ Premium", clase: "seccion-premium" },
-  basica: { titulo: "Masajistas", clase: "seccion-basica" },
+basica: { titulo: "", clase: "seccion-basica" },
 }
 
 function iniciales(nombre) {
